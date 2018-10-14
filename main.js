85823ea890672eea644d9402d5c33d0292867c93
pnwjce <elliott.jess@gmail.com>
2018-10-13 23:06:32 -0700

    started working, not able to get done with labs today, migraine

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
+'use strict';
+
+//Global variables
+var sampleProdImageLeft = document.getElementById('left');
+var sampleProdImageMid = document.getElementById('middle');
+var sampleProdImageRight = document.getElementById('right');
+var imageSection = document.getElementById('click-here');
+
+var sampleProdImageLeftArrayIndex = 0;
+
+//Images array
+var allProdImages = [];
+
+//Constructor function
+var ProdImage = function(src, name, size) {
+  this.src = src;
+  this.name = name;
+  this.likes = 0;
+  this.shown = 0;
+  this.size = size;
+  if(!size){
+    this.size = '300px';
+  }
+  allProdImages.push(this);
+};
+
+//prototypes
+ProdImage.prototype.renderProd = function() {
+  sampleProdImageLeft.src = this.src;
+};
+
+//event listeners
+var prodClickHander = function()
+
+// var busMallContainer = document.getElementById('busmall-container');
+
+// var ctx = document.getElementById('myChart').getContext('2d');
+
+// var chartData = {
+//   label:
+//   datasets:
+
+// };
+
+// var chartOptions = 
+
+// var barChart = 
 
