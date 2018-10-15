131a7b2aa7a04ecd94deb3a3120d46d59ab13ad9
pnwjce <elliott.jess@gmail.com>
2018-10-15 12:26:09 -0700

    added to js and css

--- main.js
+++ main.js
@@ -5,22 +5,23 @@ var sampleProdImageLeft = document.getElementById('left');
 var sampleProdImageMid = document.getElementById('middle');
 var sampleProdImageRight = document.getElementById('right');
 var imageSection = document.getElementById('click-here');
-
+var sampleProdLeftText = document.getElementById('prod-text-left');
+var sampleProdMidText = document.getElementById('prod-text-middle');
+var sampleProdRightText = document.getElementById('prod-text-right');
 var sampleProdImageLeftArrayIndex = 0;
+var sampleProdImageMidArrayIndex = 0;
+var sampleProdImageRightArrayIndex = 0;
+
 
 //Images array
 var allProdImages = [];
 
 //Constructor function
-var ProdImage = function(src, name, size) {
+var ProdImage = function(src, name) {
   this.src = src;
   this.name = name;
   this.likes = 0;
   this.shown = 0;
-  this.size = size;
-  if(!size){
-    this.size = '300px';
-  }
   allProdImages.push(this);
 };
 
@@ -30,7 +31,23 @@ ProdImage.prototype.renderProd = function() {
 };
 
 //event listeners
-var prodClickHander = function()
+var prodClickHander = function(eventObject) {
+  do {
+    var randomNumber = Math.floor(Math.random() * allProdImages.length);
+  } while(randomNumber === sampleProdImageLeftArrayIndex);
+
+  allProdImages[sampleProdImageLeftArrayIndex].likes++;
+  allProdImages[sampleProdImageLeftArrayIndex].shown++;
+
+  sampleProdImageLeftArrayIndex = randomNumber;
+  eventObject.target.src = allProdImages[randomNumber].src;
+};
+
+sampleProdImageLeft.addEventListener('click', prodClickHander);
+
+//new ProdImage()
+
+
 
 // var busMallContainer = document.getElementById('busmall-container');
 
