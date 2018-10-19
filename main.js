db2cd2fbc3359b309db035969504c8a5b9f928ff
pnwjce <elliott.jess@gmail.com>
2018-10-18 21:23:12 -0700

    Finished adding Products, finished with Lab 11, minor bugs to work out.

--- main.js
+++ main.js
@@ -31,26 +31,44 @@ ProdImage.prototype.renderProd = function() {
 };
 
 //event listeners
-var prodClickHander = function(eventObject) {
-  console.log('in the click handler');
+var prodClickHander = function(event) {
   do {
-    console.log('all product length' + allProdImages.length);
     var randomNumber = Math.floor(Math.random() * allProdImages.length);
   } while(randomNumber === sampleProdImageLeftArrayIndex || randomNumber === sampleProdImageMidArrayIndex || randomNumber === sampleProdImageRightArrayIndex);
-  console.log(randomNumber);
+  do {
+    var randomNumber2 = Math.floor(Math.random() * allProdImages.length);
+  } while(randomNumber2 === sampleProdImageLeftArrayIndex || randomNumber2 === sampleProdImageMidArrayIndex || randomNumber2 === sampleProdImageRightArrayIndex || randomNumber2 === randomNumber);
+  do {
+    var randomNumber3 = Math.floor(Math.random() * allProdImages.length);
+  } while(randomNumber3 === sampleProdImageLeftArrayIndex || randomNumber3 === sampleProdImageRightArrayIndex || randomNumber3 === sampleProdImageRightArrayIndex || randomNumber3 === randomNumber2 || randomNumber3 === randomNumber);
 
-  allProdImages[sampleProdImageLeftArrayIndex].likes++;
+  //tracking likes and shown
+  if(event.target.id === 'left') {
+    allProdImages[sampleProdImageLeftArrayIndex].likes++;
+    console.log('left was clicked');
+  } else if (event.target.id === 'middle') {
+    allProdImages[sampleProdImageMidArrayIndex].likes++;
+    console.log('middle was clicked');
+  } else {
+    allProdImages[sampleProdImageRightArrayIndex].likes++;
+    console.log('right was clicked');
+  }
+  
   allProdImages[sampleProdImageLeftArrayIndex].shown++;
-
-  allProdImages[sampleProdImageMidArrayIndex].likes++;
   allProdImages[sampleProdImageMidArrayIndex].shown++;
-
-  allProdImages[sampleProdImageRightArrayIndex].likes++;
   allProdImages[sampleProdImageRightArrayIndex].shown++;
 
-
   sampleProdImageLeftArrayIndex = randomNumber;
-  eventObject.target.src = allProdImages[randomNumber].src;
+  sampleProdImageMidArrayIndex = randomNumber2;
+  sampleProdImageRightArrayIndex = randomNumber3;
+
+  //event.target.src = allProdImages[randomNumber].src;
+  sampleProdImageLeft.src = allProdImages[randomNumber].src;
+  sampleProdImageMid.src = allProdImages[randomNumber2].src;
+  sampleProdImageRight.src = allProdImages[randomNumber3].src;
+  sampleProdLeftText.textContent = allProdImages[randomNumber].name;
+  sampleProdMidText.textContent = allProdImages[randomNumber2].name;
+  sampleProdRightText.textContent = allProdImages[randomNumber3].name;
 
 };
 
@@ -61,22 +79,22 @@ new ProdImage('./img/banana.jpg', 'Banana Slicer');
 new ProdImage('./img/bathroom.jpg', 'Tablet Toilet paper holder');
 new ProdImage('./img/boots.jpg', 'Toeless Rainboots');
 new ProdImage('./img/breakfast.jpg', 'Toast oven/Coffee Maker');
+new ProdImage('./img/bubblegum.jpg', 'Meatball Bubblegum');
+new ProdImage('./img/chair.jpg', 'Red Chair');
+new ProdImage('./img/cthulhu.jpg', 'Cthulhu Action Figure');
+new ProdImage('./img/dog-duck.jpg', 'Doggie Duck Mask');
+new ProdImage('./img/dragon.jpg', 'Dragon Meat');
+new ProdImage('./img/pen.jpg', 'Pen Cap Utensils');
+new ProdImage('./img/pet-sweep', 'Pen Sweeper Booties');
+new ProdImage('./img/scissors.jpg', 'Pizza Scissors');
+new ProdImage('./img/shark.jpg', 'Shark Sleeping Bag');
+new ProdImage('./img/sweep.png', 'Baby Sweeper Sleeper');
+new ProdImage('./img/tautaun.jpg', 'Tauntaun Sleeping Bag');
+new ProdImage('./img/unicorn.jpg', 'Unicorn Meat');
+new ProdImage('./img/usb.gif', 'Octopus USB stick');
+new ProdImage('./img/water-can.jpg', 'Self-Watering Can');
+new ProdImage('./img/wine-glass', 'Closed Top Wine Glass');
+
 
 console.log(allProdImages);
-
-
-
-// var busMallContainer = document.getElementById('busmall-container');
-
-// var ctx = document.getElementById('myChart').getContext('2d');
-
-// var chartData = {
-//   label:
-//   datasets:
-
-// };
-
-// var chartOptions = 
-
-// var barChart = 
 
